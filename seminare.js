0f684eb3440e967cd9f72c80b0f133239c8e64ba
RPietzschmann <72706371+RPietzschmann@users.noreply.github.com>
2021-05-16 17:31:30 +0200

    test 004
    Test

---
+++ seminare.js
@@ -0,0 +1,34 @@
+    var elementID = ""; 
+    
+    
+    function openForm(elementID) {
+        document.getElementById(elementID).style.display = "block";
+    }
+    
+    function closeForm(elementID) {
+        document.getElementById(elementID).style.display = "none";
+    }
+/*
+    function openForm2() {
+        document.getElementById("myForm2").style.display = "block";
+    }
+        
+    function closeForm2() {
+        document.getElementById("myForm2").style.display = "none";
+    }
+
+    function openForm3() {
+        document.getElementById("myForm3").style.display = "block";
+    }
+        
+    function closeForm3() {
+        document.getElementById("myForm3").style.display = "none";
+    }
+
+    function openForm4() {
+        document.getElementById("myForm4").style.display = "block";
+    }
+        
+    function closeForm4() {
+        document.getElementById("myForm4").style.display = "none";
+    }*/
